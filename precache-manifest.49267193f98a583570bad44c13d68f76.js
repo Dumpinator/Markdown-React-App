e56e6bab9de7bfeae3b9e1d258659a2acb67e5cc
Jonathan de BOISVILLIERS <jdeboisvilliers@gmail.com>
2020-09-30 10:29:14 +0200

    Updates

---
+++ precache-manifest.49267193f98a583570bad44c13d68f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4bd12d825ffafad34f85a4bcbe2382",
+    "url": "/Markdown-react-app/index.html"
+  },
+  {
+    "revision": "d671d8bede7e2f7da7c9",
+    "url": "/Markdown-react-app/static/css/main.3becd156.chunk.css"
+  },
+  {
+    "revision": "ed77ef0929b808d29eeb",
+    "url": "/Markdown-react-app/static/js/2.9d9fc09a.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/Markdown-react-app/static/js/2.9d9fc09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d671d8bede7e2f7da7c9",
+    "url": "/Markdown-react-app/static/js/main.16d77913.chunk.js"
+  },
+  {
+    "revision": "20e251e9dc2c742727ac",
+    "url": "/Markdown-react-app/static/js/runtime-main.7ec77739.js"
+  }
+]);
